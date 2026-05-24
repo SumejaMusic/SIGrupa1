f104d68500f0b7d3cade5ecb4424fef7661744f2
Kenan <Kenan@DESKTOP-IM4V23A>
2026-05-24 18:17:18 +0200

    removed console log

--- PROJEKAT/bolnicki-sistem/client/src/components/TerminDetalj.tsx
+++ PROJEKAT/bolnicki-sistem/client/src/components/TerminDetalj.tsx
@@ -27,8 +27,6 @@ import {
 } from "../utils/rezervacijeUtils";
 
 const apiUrl = import.meta.env.VITE_API_URL;
-console.log("API URL:", apiUrl);
-
 
 const otvoriNalazPDF = async (nalazId: number) => {
   try {
